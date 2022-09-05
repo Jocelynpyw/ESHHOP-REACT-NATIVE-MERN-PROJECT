6e8fe3511fe4364bf41b0c7b31350a69f180769b
jocelyn pyw <jocelynwotcheu2@gmail.com>
2022-09-05 07:28:27 +0100

    delete or orderItem for a order

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -4,11 +4,30 @@ const mongoose = require("mongoose");
 const OrderItemModel = require("../models/order-item");
 
 module.exports.getOrdersList = expressAsyncHandler(async (req, res) => {
-  const userList = await OrderModel.find();
-
+  // .Populate fait une jointure entre les differentes tables , ici grace a l'id du User jee peux afficher ses informations dans mon Order
+  const userList = await OrderModel.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
+  // Juste la haut je ne veux populate que le nom du user
   if (!userList) return res.status(500).json({ success: false });
 
   res.send(userList);
+});
+
+module.exports.getOneOrder = expressAsyncHandler(async (req, res) => {
+  const order = await OrderModel.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
+
+  if (!order)
+    return res
+      .status(500)
+      .json({ success: false, message: "the Order does not exist" });
+
+  res.send(order);
 });
 
 module.exports.createOrder = expressAsyncHandler(async (req, res) => {
@@ -57,4 +76,51 @@ module.exports.createOrder = expressAsyncHandler(async (req, res) => {
   if (!order) return res.status(500).send("your User cannot be create");
 
   res.send(order);
+});
+
+module.exports.updateStatusOrder = expressAsyncHandler(async (req, res) => {
+  let id = req.params.id;
+  let status = req.body.status;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).send("Invalid Order Id");
+  }
+
+  const order = await OrderModel.findByIdAndUpdate(
+    id,
+    {
+      status: status,
+    },
+    { new: true }
+  );
+  if (!order) return res.status(400).send("the Order cannot be created !");
+
+  res.send(order);
+});
+
+module.exports.deleteOrder = expressAsyncHandler(async (req, res) => {
+  let id = req.params.id;
+  OrderModel.findByIdAndRemove(id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItemModel.findByIdAndRemove(orderItem);
+        });
+        return res.status(200).json({
+          success: true,
+          message: "The order is deleted",
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "order not found",
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        success: false,
+        error: err,
+      });
+    });
 });
--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const orderController = require("../controllers/order.controller");
 
 router.get("/", orderController.getOrdersList);
+router.get("/:id", orderController.getOneOrder);
+router.put("/:id", orderController.updateStatusOrder);
+router.delete("/:id", orderController.deleteOrder);
 router.post("/", orderController.createOrder);
 
 module.exports = router;
