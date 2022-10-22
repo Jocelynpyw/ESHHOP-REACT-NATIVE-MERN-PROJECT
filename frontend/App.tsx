f382175a501ca159744df4e58ea49b60d9b0fd97
jocelyn pyw <jocelynwotcheu2@gmail.com>
2022-10-22 07:13:23 +0100

    ajout de redux et react redux

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {Provider} from 'react-redux';
 import Main from './src/navigation/main';
+import store from './src/redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
